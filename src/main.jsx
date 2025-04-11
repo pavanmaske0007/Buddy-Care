41e98749896231e316207f55d7bc8e2c50eaab56
pavanmaske0007 <maskepavan2151@gmail.com>
2025-04-11 08:16:25 +0530

    Buddy-Care

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-// import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App.jsx";
+// import react from "react";
+// // import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { HashRouter } from "react-router-dom";
+// import "./index.css";
+// import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+// createRoot(document.getElementById("root")).render(
+//   <HashRouter>
+//     <App />
+//   </HashRouter>
+// );
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
     <App />
   </HashRouter>
